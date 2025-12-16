e99e8e72687e55b7da8344d591c0396e0bc3b7a7
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-12-16 02:12:29 -0600

    Bump cache version to acpay-v19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v18'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v19'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
