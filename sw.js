542275e8bf4d8b6ca1f7223eaa6e0eb3fd77708e
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-12-16 23:28:18 -0600

    Bump cache version to v24
    Updated cache version to v24 for asset management.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v23'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v24'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
