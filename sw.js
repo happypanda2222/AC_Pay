148a3dab4891b3e5cfb628d7db23b7a65d1386f4
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-12-17 01:02:03 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v30'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v31'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
