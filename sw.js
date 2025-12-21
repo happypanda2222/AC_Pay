8b2414a485f7244c0f41f2c89143b62c4860c0ab
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-12-21 09:18:02 -0600

    Add disruptive schedule tooltip

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v96'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v97'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
