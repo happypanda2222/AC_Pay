b0a0ea7ad10b6c3c6c955a39c2b9d413a99ac811
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-12-24 12:02:21 -0600

    Handle Cuban METAR visibility units

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v130'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v131'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
