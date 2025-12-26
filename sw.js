8989550699e327b9ebfcf0c6c702b53b6f625ff1
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-12-25 22:11:39 -0600

    Update Easter egg accent styles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v134'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v135'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
