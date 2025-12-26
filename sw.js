9c6dc7cb7c1bca86d20772270c7fd983b74ec5f9
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-12-25 21:50:30 -0600

    Extend Easter egg red styling to tabs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v132'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v134'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
