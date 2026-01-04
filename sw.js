b97bb3203be274f4758fd37a95820c8e78aae408
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-03 23:27:51 -0800

    Ensure auth headers bypass proxy fallbacks

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v203'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v204'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
