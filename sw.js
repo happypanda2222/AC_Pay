b169b42a4d477984cb122326c1eb16abc35e091c
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-18 23:34:56 -0600

    Update CSP for calendar sync worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v301'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v302'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
