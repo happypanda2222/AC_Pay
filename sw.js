7716cc3bb79ac36ae4d1ae133aed0b41f2deb5b9
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-20 00:03:58 -0600

    Add overnight checkout placeholders

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v350'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v351'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
