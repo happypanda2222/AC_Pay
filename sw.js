1583c0f5638d3db3ffed34e0c8207410647d50ff
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-20 01:18:17 -0600

    Make pairing IDs unique per parse

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v360'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v361'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
