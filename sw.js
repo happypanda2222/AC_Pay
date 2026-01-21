94bc20f99b2229b897d2b41b63459192c5cec9d9
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-21 15:52:02 -0600

    Reset pairing THG using full day list

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v407'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v408'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
