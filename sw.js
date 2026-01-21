fcdbb52f4ebe72abbe8a38c7e5afa86a1c964723
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-21 15:31:07 -0600

    Adjust calendar week row label layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v403'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v404'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
