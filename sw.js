da829bb5e98a1c57a77701f836888e0b6b6e398b
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-22 15:23:35 -0500

    Save calendar month preference without sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v464'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v465'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
