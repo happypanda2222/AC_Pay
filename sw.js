fc1725860690a1340c8d3766d1689204a11b04ac
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-21 21:44:03 -0500

    Lower calendar hotel bar stacking

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v434'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v435'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
