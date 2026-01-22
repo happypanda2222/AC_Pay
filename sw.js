712f60a7a159ed840dd34fd7d457739bfd394511
happypanda2222 <tripods.scrawls1n@icloud.com>
2026-01-21 19:22:55 -0600

    Adjust calendar hotel bar layering

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'acpay-v424'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
+const CACHE = 'acpay-v425'; // ⬅️ bump this (v2, v3, ...) whenever you change assets
 const ASSETS = [
   './',
   './index.html',
